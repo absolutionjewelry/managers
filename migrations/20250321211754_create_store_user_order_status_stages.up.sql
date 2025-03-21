e238dc55ab77a124abcc1d2d84cf095e85fd081f
Michael Christenson II <m3talsmith@gmail.com>
2025-03-21 18:03:30 -0400

    Adjusted stages to have position

--- migrations/20250321211754_create_store_user_order_status_stages.up.sql
+++ migrations/20250321211754_create_store_user_order_status_stages.up.sql
@@ -2,16 +2,18 @@
 CREATE TABLE store_user_order_status_stage_types (
     id VARCHAR(255) NOT NULL PRIMARY KEY,
     store_id VARCHAR(255) NOT NULL REFERENCES stores(id),
-    name VARCHAR(255) NOT NULL,
-    description TEXT,
+    stage_name VARCHAR(255) NOT NULL,
+    stage_description TEXT,
+    stage_position INT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     archived_at TIMESTAMP,
-    UNIQUE (store_id, name)
+    UNIQUE (store_id, stage_name)
 );
 
 CREATE INDEX idx_store_user_order_status_stage_types_store_id ON store_user_order_status_stage_types(store_id);
-CREATE INDEX idx_store_user_order_status_stage_types_name ON store_user_order_status_stage_types(name);
+CREATE INDEX idx_store_user_order_status_stage_types_stage_name ON store_user_order_status_stage_types(stage_name);
+CREATE INDEX idx_store_user_order_status_stage_types_stage_position ON store_user_order_status_stage_types(stage_position);
 CREATE INDEX idx_store_user_order_status_stage_types_created_at ON store_user_order_status_stage_types(created_at);
 CREATE INDEX idx_store_user_order_status_stage_types_updated_at ON store_user_order_status_stage_types(updated_at);
 CREATE INDEX idx_store_user_order_status_stage_types_archived_at ON store_user_order_status_stage_types(archived_at);
